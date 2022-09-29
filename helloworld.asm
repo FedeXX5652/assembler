1af2d4441b78105f1bf948efc662502d6c242975
FedeXX5652 <fedegalant@gmail.com>
2022-09-28 21:09:13 -0300

    hello world

---
+++ helloworld.asm
@@ -0,0 +1,22 @@
+bits 64
+default rel
+
+section .data
+    msg db "Hello world", 0xd, 0xa, 0
+
+section .text
+global main
+extern ExitProcess
+
+extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+
+    lea rcx, [msg]
+    call printf
+
+    xor rax, rax
+    call ExitProcess
